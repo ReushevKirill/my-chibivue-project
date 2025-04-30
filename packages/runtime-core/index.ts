51e4338c07231d325f25cb95383512ef41934bf2
ReushevKirill <reushev.kd@gmail.com>
2025-04-30 13:47:02 +0300

    first commit

---
+++ packages/runtime-core/index.ts
@@ -0,0 +1,7 @@
+import type { RendererOptions } from './renderer'
+import { createRenderer } from './renderer'
+import { h } from './h'
+import type { VNode, VNodeProps } from './vnode'
+
+export type { RendererOptions, VNode, VNodeProps }
+export { createRenderer, h }
---
+++ packages/runtime-dom/index.ts
@@ -0,0 +1,18 @@
+import { createRenderer } from '../runtime-core'
+import { createAppAPI, CreateAppFunction } from '../runtime-core/apiCreateApp'
+import { nodeOps } from './nodeOps'
+
+const { render } = createRenderer(nodeOps)
+const _createApp = createAppAPI(render)
+
+export const createApp = ((...args) => {
+	const app = _createApp(...args)
+	const { mount } = app
+	app.mount = (selector: string) => {
+		const container = document.querySelector(selector)
+		if (!container) return
+		mount(container)
+	}
+
+  return app
+}) as CreateAppFunction<Element>
