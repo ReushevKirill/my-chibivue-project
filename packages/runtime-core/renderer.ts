0e1d471ad7e5e777988c35ecdeb3cb73d97e0865
ReushevKirill <reushev.kd@gmail.com>
2025-06-07 23:00:46 +0300

    vdom1

--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -1,4 +1,5 @@
-import { VNode } from './vnode'
+import { ReactiveEffect } from '../reactivity/effect'
+import { normalizeVNode, Text, VNode, VNodeProps } from './vnode'
 
 export interface RendererOptions<
 	HostNode = RendererNode,
@@ -9,6 +10,8 @@ export interface RendererOptions<
 	createElement(type: string): HostNode
 
 	createText(text: string): HostNode
+
+	setText(node: HostNode, text: string): void
 
 	setElementText(node: HostNode, text: string): void
 
@@ -31,6 +34,7 @@ export function createRenderer(options: RendererOptions) {
 		patchProp: hostPatchProp,
 		createElement: hostCreateElement,
 		createText: hostCreateText,
+		setText: hostSetText,
 		insert: hostInsert,
 	} = options
 
@@ -50,10 +54,102 @@ export function createRenderer(options: RendererOptions) {
 		return el
 	}
 
-	const render: RootRenderFunction = (vnode, container) => {
-		while (container.firstChild) container.removeChild(container.firstChild)
-		const el = renderVNode(vnode)
+	const patch = (n1: VNode | null, n2: VNode, container: RendererElement) => {
+		const { type } = n2
+		if (type === Text) {
+			processText(n1, n2, container)
+		} else {
+			processElement(n1, n2, container)
+		}
+	}
+
+	const processElement = (
+		n1: VNode | null,
+		n2: VNode,
+		container: RendererElement
+	) => {
+		if (n1 === null) {
+			mountElement(n2, container)
+		} else {
+			patchElement(n1, n2)
+		}
+	}
+
+	const mountElement = (vnode: VNode, container: RendererElement) => {
+		let el: RendererElement
+		const { type, props } = vnode
+		el = vnode.el = hostCreateElement(type as string)
+
+		mountChildren(vnode.children, el)
+
+		if (props) {
+			for (const key in props) {
+				hostPatchProp(el, key, props[key])
+			}
+		}
+
 		hostInsert(el, container)
+	}
+
+	const patchElement = (n1: VNode, n2: VNode) => {
+		const el = (n2.el = n1.el!)
+
+		const props = n2.props
+
+		patchChildren(n1, n2, el)
+
+		for (const key in props) {
+			if (props[key] !== n1.props?.[key]) {
+				hostPatchProp(el, key, props[key])
+			}
+		}
+	}
+
+	const patchChildren = (n1: VNode, n2: VNode, container: RendererElement) => {
+		const c1 = n1.children as VNode[]
+		const c2 = n2.children as VNode[]
+
+		for (let i = 0; i < c2.length; i++) {
+			const child = (c2[i] = normalizeVNode(c2[i]))
+			patch(c1[i], child, container)
+		}
+	}
+
+	const mountChildren = (children: VNode[], container: RendererElement) => {
+		for (let i = 0; i < children.length; i++) {
+			const child = (children[i] = normalizeVNode(children[i]))
+			patch(null, child, container)
+		}
+	}
+
+	const processText = (
+		n1: VNode | null,
+		n2: VNode,
+		container: RendererElement
+	) => {
+		if (n1 == null) {
+			hostInsert((n2.el = hostCreateText(n2.children as string)), container)
+		} else {
+			const el = (n2.el = n1.el!)
+			if (n2.children !== n1.children) {
+				hostSetText(el, n2.children as string)
+			}
+		}
+	}
+
+	const render: RootRenderFunction = (rootComponent, container) => {
+		const componentRender = rootComponent.setup!()
+
+		let n1: VNode | null = null
+
+		const updateComponent = () => {
+			const n2 = componentRender()
+			patch(n1, n2, container)
+			n1 = n2
+		}
+
+		const effect = new ReactiveEffect(updateComponent)
+		effect.run()
 	}
 
 	return { render }
--- packages/runtime-core/vnode.ts
+++ packages/runtime-core/vnode.ts
@@ -1,9 +1,41 @@
-export interface VNode {
-	type: string
-	props: VNodeProps
-	children: (VNode | string)[]
+export const Text = Symbol()
+
+export type VNodeTypes = string | typeof Text
+
+export interface VNode<HostNode = any> {
+	type: VNodeTypes
+	props: VNodeProps | null
+	children: VNodeNormalizedChildren
+
+	el: HostNode | undefined
 }
 
 export interface VNodeProps {
 	[key: string]: any
+}
+
+// Тип после нормализации
+export type VNodeNormalizedChildren = string | VNodeArrayChildren
+export type VNodeArrayChildren = Array<VNodeArrayChildren | VNodeChildAtom>
+
+export type VNodeChild = VNodeChildAtom | VNodeArrayChildren
+type VNodeChildAtom = VNode | string
+
+export function createVNode(
+	type: VNodeTypes,
+	props: VNodeProps | null,
+	children: VNodeNormalizedChildren
+): VNode {
+	const vnode: VNode = { type, props, children, el: undefined }
+	return vnode
+}
+
+// функция normalize (используется в renderer.ts)
+export function normalizeVNode(child: VNodeChild): VNode {
+	if (typeof child === 'object') {
+		return { ...child } as VNode
+	} else {
+		// Преобразуем строку в желаемую форму, представленную ранее
+		return createVNode(Text, null, String(child))
+	}
 }
